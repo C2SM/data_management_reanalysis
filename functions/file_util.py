8cda4b40747e8aae0b1b764655d570673420cdb3
ruthlorenz <ruth.lorenz22@gmail.com>
2024-03-28 14:12:28 +0100

    changes using isort, black

--- functions/file_util.py
+++ functions/file_util.py
@@ -1,56 +1,59 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
+"""
 File Name : file_util.py
 Author: Ruth Lorenz (ruth.lorenz@c2sm.ethz.ch)
 Created: 27/03/2024
 Purpose: utility functions to process ERA5 data downloaded from dkrz
-'''
-import os
+"""
 import json
+import os
+
 
 class Config:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-def read_config(configfolder, configfile):
 
+def read_config(configfolder, configfile):
     config = {}
-    type_mapping = {'True': True, 'False': False}
-    with open(os.path.join(os.getcwd(), configfolder, configfile), 'r') as f:
+    type_mapping = {"True": True, "False": False}
+    with open(os.path.join(os.getcwd(), configfolder, configfile), "r") as f:
         for line in f:
-            if '=' in line:
-                k,v = line.split('=', 1)
-                v = v.replace('"', '').strip()
-                if ',' in v:
-                    v = [item.strip() for item in v.split(',')] 
-                elif ' ' in v:
-                    v = v.split(' ')              
+            if "=" in line:
+                k, v = line.split("=", 1)
+                v = v.replace('"', "").strip()
+                if "," in v:
+                    v = [item.strip() for item in v.split(",")]
+                elif " " in v:
+                    v = v.split(" ")
                 else:
                     v = type_mapping.get(v, int(v) if v.isdigit() else v)
                 config[k.strip()] = v
 
     return Config(**config)
 
+
 # -------------------------------------------------
 # Read ERA5 info from JSON file
 # -------------------------------------------------
 
+
 def read_era5_info(vname):
-    '''
-    Loading ERA5 variables's information as 
+    """
+    Loading ERA5 variables's information as
     python Dictionary from JSON file
 
     Input:
     a string with the ERA5 variable short name to be processed
- 
+
     Return:
     dict with variable infos
-    '''
+    """
     era5_info = dict()
 
-    with open('ERA5_variables.json', 'r') as jf:
+    with open("ERA5_variables.json", "r") as jf:
         era5 = json.load(jf)
         # Variable's long-name, param and unit
         vlong = era5[vname][0]
@@ -69,5 +72,3 @@ def read_era5_info(vname):
         era5_info["cmip_unit"] = unitcmip
 
     return era5_info
-
-
