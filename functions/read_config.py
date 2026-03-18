7734fcc955fa9332b374339c4ec7e59c2c6cf553
Ruth Lorenz <ruth.lorenz@c2sm.ethz.ch>
2026-03-18 12:13:15 +0100

    updated read_confif to be able to either read absolute years to be processed from yaml file or a relative delta, which downloads/processes data from X months prior. this was implemented for continuous updates.

--- functions/read_config.py
+++ functions/read_config.py
@@ -1,4 +1,7 @@
 import yaml
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 
 def read_yaml_config(file_path):
     """
@@ -13,7 +16,25 @@ def read_yaml_config(file_path):
     try:
         with open(file_path, 'r') as file:
             config = yaml.safe_load(file)
+        d = config.get("time", {})
+        t_month = d.get("months")
+        start_year = d.get("startyr")
+        end_year = d.get("endyr")
+        offset = d.get("offset_months", 0)
+
+        # Logic: Use hardcoded values if BOTH are present
+        if start_year and end_year:
+            print("Using hardcoded date from YAML.")
             return config
+
+        # Fallback: Calculate based on offset
+        print(f"Calculating date using offset: -{offset} months.")
+        target_date = datetime.now() - relativedelta(months=offset)
+        config['time']['months'] = target_date.month
+        config['time']['startyr'] = target_date.year
+        config['time']['endyr'] = target_date.year
+
+        return config
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' does not exist.")
         return None
