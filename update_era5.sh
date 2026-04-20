19c30f3a6bfface211a77ce27ab5c848ac5c2e8d
Ruth Lorenz <ruth.lorenz@c2sm.ethz.ch>
2026-04-20 15:06:57 +0200

    fix update_era5.sh bash script to use correct environment (environment not available in subshell using source activate)

--- update_era5.sh
+++ update_era5.sh
@@ -6,6 +6,8 @@
 # Purpose : update era5 data,
 #	either multiple months and years or
 #	one month defined by offset_months X months ago
+#   defined in the config file, e.g. if current month is 2026-03 and offset_months is 4,
+#   then data from 2025-11 will be downloaded and processed
 
 ###-------------------------------------------------------
 
@@ -14,41 +16,43 @@ printf -v date '%(%Y-%m-%d_%H%M%S)T' -1
 logfile="update_era5_$date.log"
 mkdir -p logfiles
 {
-export PYTHONPATH=""
-module load conda
-source activate iacpy3_2025
+
+PYTHON_EXE=/usr/local/Miniconda3-envs/envs/2025/envs/iacpy3_2025/bin/python
+echo $PYTHON_EXE
+python -c "import sys; print(sys.executable)"
 
 # update daily 2D variables at surface available at DKRZ
-variable_list=(tp strd ssrd str cbh sst msl u10 v10 2t 2d skt sp)
+variable_list=("tp" "strd" "ssrd" "str" "cbh" "sst" "msl" "u10" "v10" "2t" "2d" "skt" "sp")
 #variable_list=(tp)
 for var in "${variable_list[@]}"; do
-    python process_2D_from_dkrz_or_cds_daily_files.py -c configs/Config_era5_1day_sf_dkrz.yaml -v "$var"
-
+    echo $var
+    $PYTHON_EXE process_2D_from_dkrz_or_cds_daily_files.py -c configs/Config_era5_1day_sf_dkrz.yaml -v $var
 done
 
 # update daily 1D variables at surface available only on CDS
-variable_list=(tcc)
+variable_list=("tcc")
 for var in "${variable_list[@]}"; do
-    python process_2D_from_dkrz_or_cds_daily_files.py -c configs/Config_era5_1day_sf_cds.yaml -v "$var"
+    echo $var
+    $PYTHON_EXE process_2D_from_dkrz_or_cds_daily_files.py -c configs/Config_era5_1day_sf_cds.yaml -v $var
 done
 
 # update daily 2D variables calculated from hourly files at DKRZ
 # variable_list=(tasmax, tasmin)
-python process_2D_maxmin_from_dkrz_hourly_files.py -c configs/Config_era5_2t_minmax_dkrz.yaml
+$PYTHON_EXE process_2D_maxmin_from_dkrz_hourly_files.py -c configs/Config_era5_2t_minmax_dkrz.yaml
 
 # variable_list=(sfcWind)
-python process_2D_sfcWind_from_dkrz_hourly_files.py -c configs/Config_era5_1day_sfcWind_dkrz.yaml
+$PYTHON_EXE process_2D_sfcWind_from_dkrz_hourly_files.py -c configs/Config_era5_1day_sfcWind_dkrz.yaml
 
 # update daily 3D variables available from DKRZ
-variable_list=(q r t u v)
+variable_list=("q" "r" "t" "u" "v")
 for var in "${variable_list[@]}"; do
-    python process_3D_from_dkrz_daily_files.py -c configs/Config_era5_1day_pl_dkrz.yaml -v "$var"
+    $PYTHON_EXE process_3D_from_dkrz_daily_files.py -c configs/Config_era5_1day_pl_dkrz.yaml -v $var
 done
 
 # update z from hourly files at dkrz
-variable_list=(z)
+variable_list=("z")
 for var in "${variable_list[@]}"; do
-    python process_3D_z_from_dkrz_hourly_files.py -c configs/Config_era5_1hr_pl_z_dkrz.yaml -v "$var"
+    $PYTHON_EXE process_3D_z_from_dkrz_hourly_files.py -c configs/Config_era5_1hr_pl_z_dkrz.yaml -v $var
 done
 
-} 2>&1 | tee logfiles/"$logfile"
+} 2>&1 | tee logfiles/${logfile}
