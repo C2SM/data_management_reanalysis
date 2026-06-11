bbe796d2907a10df97f9e20dd6afc111d89fae8d
Ruth Lorenz <ruth.lorenz@c2sm.ethz.ch>
2026-06-11 15:17:14 +0200

    extraction of variables not necessary, instead unzip downloaded zip file which contains single files per variable

--- process_2D_analysis_multvars_from_cds_daily.py
+++ process_2D_analysis_multvars_from_cds_daily.py
@@ -101,8 +101,20 @@ def read_era5_info(vname_list):
     return era5_info
 
 
-def download_data_cds_multvar(dataname, varlist, download_path, workdir, year, month, statistic="daily_mean"):
-    target = f'{workdir}/variables_{statistic}_{dataname}_{year}{month}.nc'
+def download_data_cds_multvar(dataname, varlist, download_path, year, month, statistic="daily_mean"):
+    '''
+    Download data from CDS for given variables, year and month, and save to target file.
+    It will download all variables in varlist in one request, and save to one zip file by default.
+
+    Input:
+    dataname: name of the dataset, e.g. "ERA5-Land"
+    varlist: list of variable long names to be downloaded, e.g. ["2m temperature", "10m u-component of wind"]
+    download_path: path to save the downloaded file
+    year: year to be downloaded, e.g. 2020
+    month: month to be downloaded, e.g. "01" for January
+    statistic: daily statistic to be downloaded, e.g. "daily_mean", "daily_maximum", "daily_minimum"
+    '''
+    target = f'{download_path}/variables_{statistic}_{dataname}_{year}{month}.zip'
 
     if not os.path.isfile(f'{target}') or overwrite:
         logger.info(f"Downloading data for {dataname} from CDS for year {year} and month {month}.")
@@ -212,33 +224,30 @@ def main():
         if store == 'cds':
             for month in months:
                 # reteive data from cds, returns path to downloaded file, all variables in one file
-                download_file_multvar = download_data_cds_multvar(dataname, varlist_long, download_path, work_path, year, month, statistic)
+                download_file_multvar = download_data_cds_multvar(dataname, varlist_long, download_path, year, month, statistic)
                 # process the downloaded file
+                # 1. unzip into single variable files
+                with zipfile.ZipFile(download_file_multvar, 'r') as zip_ref:
+                    zip_ref.extractall(work_all_path)
                 for v, var in enumerate(var_list_short):
                     proc_archive = f'{proc_path}/{var}/day/native/{year}'
                     os.makedirs(proc_archive, exist_ok=True)
                     work_path = f"{work_all_path}/{var}/"
                     os.makedirs(work_path, exist_ok=True)
-                    # 1. extract variables
-                    #os.system(f'ncks -v {era5_info[var]["param"]} {download_file_multvar} {workdir}/{era5_info[var]["param"]}_{statistic}_era5-land_{year}{month}.nc')
-                    file = f'{work_path}/{era5_info[var]["param"]}_{statistic}_{dataname}_{year}{month}.nc'
-                    try:
-                        cmd = [
-                            'ncks',
-                            '-v', era5_info[var]["param"],
-                            download_file_multvar,
-                            file
-                        ]
-                        result = subprocess.run(cmd, check=True, capture_output=True, text=True)
-                    except subprocess.CalledProcessError as e:
-                        logger.error(f"Command failed with return code {e.returncode}")
-                        logger.error(f"Standard output:\n{e.stdout}")
+                    # 1. find file name for the variable, which should be the same as the long name in era5_info
+                    var_long = era5_info[var]['long_name']
+                    statistic_string = statistic.replace("_", "-")
+                    file = f'{work_all_path}/{var_long}_0_{statistic_string}.nc'
+                    if not os.path.isfile(file):
+                        logger.error(f"Expected file {file} for variable {var} not found after unzipping."
+                        logger.error(f"Check if the variable long name in era5_info matches the file name in the zip file.")
                         sys.exit(1)
 
                     # 2. process each variable separately, with renaming and unit conversion if necessary, and write to cmip like format
-                    outfile = f'{proc_archive}/{era5_info[var]["cmip_name"]}_{statistic}_{dataname}_{year}{month}.nc'
+                    outfile = f'{proc_archive}/{era5_info[var]["cmip_name"]}_{statistic_string}_{dataname}_{year}{month}.nc'
                     if os.path.isfile(outfile) and not overwrite:
-                        logger.info(f"File {outfile} already exists and overwrite is set to False. Skipping processing of variable {var} for month {month}.")
+                        logger.info(f"File {outfile} already exists and overwrite is set to False.")
+                        logger.info(f"Skipping processing of variable {var} for month {month}.")
                         continue
 
                     tmp_outfile = convert_valid_time_latitude_longitude(
