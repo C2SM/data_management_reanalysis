c870df893e611461b4a911cc58c70d0091f3ae90
Ruth Lorenz <ruth.lorenz@c2sm.ethz.ch>
2026-07-08 14:16:33 +0200

    need to use var{era5_info[var][param]} as original variable name in the file, more checks and fixes, shift time by -1sec because accumulated variables saved at the end of accumulated period, 00:00:00 is on the next calendar day

--- process_2D_accumulated_daily_multvar_from_cds_hourly.py
+++ process_2D_accumulated_daily_multvar_from_cds_hourly.py
@@ -89,7 +89,7 @@ def download_data_cds_multvar(dataname, varlist, download_path, year, month, ove
                 "11", "12", "13", "14", "15",
                 "16", "17", "18", "19", "20",
                 "21", "22", "23 ", "24", "25",
-                "26", "27", "28", "29", "30", "31"]
+                "26", "27", "28", "29", "30", "31"],
             "time": ["00:00"],
             "data_format": "grib",
             "download_format": "unarchived"
@@ -157,48 +157,68 @@ def extract_vars_change_metadata(nc_file, var, era5_info):
     Return:
     Saves the extracted variable to a new netcdf file with updated metadata.
     '''
+    logger.info(f'Extracting variable {var} from {nc_file} and changing metadata.')
     nc_file_path = Path(nc_file)
     basename = nc_file_path.stem
     work_out = nc_file_path.parent
 
     try:
         cmd = ['ncks',
+                '-O',
                 '-v',
-                f'{var}',
+                f'var{era5_info[var]["param"]}',
                 f'{nc_file}',
                 f'{work_out}/{var}_{basename}.nc']
-        subprocess.run(cmd, check=True)
+        result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+        returncode = result.returncode
+        logger.info(f"Command returned code {returncode}")
     except subprocess.CalledProcessError as e:
         logger.error(f"Error extracting variable {var} from {nc_file}: {e}")
-        continue
+        returncode = e.returncode
+        logger.error(f"Command failed with return code {returncode}")
+        logger.error(f"Standard output:\n{e.stdout}")
+        logger.error(f"Standard error:\n{e.stderr}")
+        sys.exit()
 
     try:
         cmd = ['ncatted',
+                '-O',
                 '-a',
-                f'standard_name,{var},c,c,{era5_info[var]['long_name']}',
+                f'standard_name,var{era5_info[var]["param"]},c,c,{era5_info[var]['long_name']}',
                 f'{work_out}/{var}_{basename}.nc',
                 f'{work_out}/{var}_{basename}_ncatted.nc']
-        subprocess.run(cmd, check=True)
+        result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+        returncode = result.returncode
     except subprocess.CalledProcessError as e:
         logger.error(f"Error adding standard_name attribute to {var}: {e}")
-        continue
+        returncode = e.returncode
+        logger.error(f"Command failed with return code {returncode}")
+        logger.error(f"Standard output:\n{e.stdout}")
+        logger.error(f"Standard error:\n{e.stderr}")
+        sys.exit()
 
     try:
         cmd = ['ncatted',
+                '-O',
                 '-a',
-                f'units,{var},c,c,"{era5_info[var]['units']}"',
+                f'units,var{era5_info[var]["param"]},c,c,"{era5_info[var]['unit']}"',
                 f'{work_out}/{var}_{basename}_ncatted.nc',
                 f'{work_out}/{var}_{basename}_ncatted2.nc']
-        subprocess.run(cmd, check=True)
+        result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+        returncode = result.returncode
     except subprocess.CalledProcessError as e:
         logger.error(f"Error adding units attribute to {var}: {e}")
-        continue
+        returncode = e.returncode
+        logger.error(f"Command failed with return code {returncode}")
+        logger.error(f"Standard output:\n{e.stdout}")
+        logger.error(f"Standard error:\n{e.stderr}")
+        sys.exit()
 
     try:
         cdo.setname(var, input=f'{work_out}/{var}_{basename}_ncatted2.nc', output=f'{work_out}/{var}_{basename}_setname.nc')
     except Exception as e:
         logger.error(f"Error setting name for {var}: {e}")
-        continue
+        sys.exit()
 
     return f'{work_out}/{var}_{basename}_setname.nc'
 
@@ -256,6 +276,12 @@ def main():
     # overwrite files, download from CDS if already exists and reprocess
     overwrite = config['flags']['overwrite']
 
+    # -------------------------------------------------
+    # read ERA5_variables.json
+    # -------------------------------------------------
+    logger.info(f"{config['dataset']['name']} variable info red from json file.")
+    era5_info = read_era5_info_list(var_list_short)
+    print(era5_info)
 
     # download and process for all years in configuration
     for year in range(startyr, endyr + 1):
@@ -264,82 +290,86 @@ def main():
 
         varlist_long = [era5_info[var]['long_name'] for var in var_list_short]
         print(f"Variable list for download: {varlist_long}")
-        if store == 'cds':
-            for month in months:
-                # reteive data from cds, returns path to downloaded file, all variables in one file
-                download_file_multvar = download_data_cds_multvar(dataname, varlist_long, download_path, year, month, overwrite=overwrite)
-                download_file_multvar_plus1 = download_data_cds_multvar_plus1(dataname, varlist_long, download_path, year, month, overwrite=overwrite)
+        assert store == 'cds', f"Store {store} is not supported."
+        for month in months:
+            # retreive data from cds, returns path to downloaded file, all variables in one file
+            download_file_multvar = download_data_cds_multvar(dataname, varlist_long, download_path, year, month, overwrite=overwrite)
+            download_file_multvar_plus1 = download_data_cds_multvar_plus1(dataname, varlist_long, download_path, year, month, overwrite=overwrite)
 
-                # convert grib file to netcdf
-                download_file_path = Path(download_file_multvar)
-                nc_file = download_file_path.replace(suffix=".nc")
+            # convert grib file to netcdf
+            download_file_path = Path(download_file_multvar)
+            nc_file = download_file_path.with_suffix(".nc")
+            print(str(nc_file))
+            try:
+                cdo.sorttaxis(options="-f nc4", input=download_file_multvar, output=str(nc_file))
+            except Exception as e:
+                logger.error(f"Error converting {download_file_multvar} to netcdf: {e}")
+                continue
+
+            # convert plus1 file to netcdf
+            download_file_plus1_path = Path(download_file_multvar_plus1)
+            nc_file_plus1 = download_file_plus1_path.with_suffix(".nc")
+            try:
+                cdo.sorttaxis(options="-f nc4", input=download_file_multvar_plus1, output=str(nc_file_plus1))
+            except Exception as e:
+                logger.error(f"Error converting {download_file_multvar_plus1} to netcdf: {e}")
+                continue
+
+            # extract individual variables and change metadata
+            for v, var in enumerate(var_list_short):
+                logger.info(f"Processing variable {var} for year {year} and month {month}.")
+                work_out = f'{work_all_path}/{var}/'
+                os.makedirs(work_out, exist_ok=True)
+                proc_archive = f'{proc_path}/{era5_info[var]["cmip_name"]}/day/native/{year}'
+                os.makedirs(proc_archive, exist_ok=True)
+
+                tmpfile = extract_vars_change_metadata(nc_file, var, era5_info)
+                tmpfile_plus1 = extract_vars_change_metadata(nc_file_plus1, var, era5_info)
+
+                # accumulated values are over the 24 hours ending at 00 UTC i.e. the accumulation is during the previous day,
+                # so we need to shift the time by -1 second to get the correct calendar day for the accumulation
+                tmp_outfile = f'{work_out}/{var}_1day_era5-land_{year}{month}.nc'
                 try:
-                    cdo.sorttaxis(option="-f nc4", input=download_file_multvar, output=nc_file)
+                    cdo.shifttime("-1sec", input=tmpfile, output=tmp_outfile)
                 except Exception as e:
-                    logger.error(f"Error converting {download_file_multvar} to netcdf: {e}")
+                    logger.error(f"Error shifting time for {tmpfile}: {e}")
+                    continue
+                tmp_outfile_plus1 = f'{work_out}/{var}_1day_era5-land_{year}{month}_plus1.nc'
+                try:
+                    cdo.shifttime("-1sec", input=tmpfile_plus1, output=tmp_outfile_plus1)
+                except Exception as e:
+                    logger.error(f"Error shifting time for {tmpfile_plus1}: {e}")
+                    continue
+                # concatenate plus1 file to the end of the month file to get the last day of the month correct
+                tmp_outfile_concat = f'{work_out}/{var}_1day_era5-land_{year}{month}_concat.nc'
+                try:
+                    cdo.mergetime(input=f"{tmp_outfile} {tmp_outfile_plus1}", output=tmp_outfile_concat)
+                except Exception as e:
+                    logger.error(f"Error concatenating {tmp_outfile} and {tmp_outfile_plus1}: {e}")
                     continue
 
-                # convert plus1 file to netcdf
-                download_file_plus1_path = Path(download_file_multvar_plus1)
-                nc_file_plus1 = download_file_plus1_path.replace(suffix=".nc")
-                try:
-                    cdo.sorttaxis(option="-f nc4", input=download_file_multvar_plus1, output=nc_file_plus1)
-                except Exception as e:
-                    logger.error(f"Error converting {download_file_multvar_plus1} to netcdf: {e}")
-                    continue
 
-                # extract individual variables and change metadata
-                for v, var in enumerate(var_list_short):
-                    work_out=f'{work_all_path}/{var}/'
-                    os.makedirs(work_out, exist_ok=True)
+                outfile = f'{proc_archive}/{era5_info[var]["cmip_name"]}_day_{dataname}_{year}{month}.nc'
+                outfile_name = convert_era5_to_cmip(
+                    tmp_outfile_concat, outfile, store, era5_info[var],
+                    config['chunking']['time_chk'], config['chunking']['lon_chk'], config['chunking']['lat_chk']
+                )
+                assert outfile_name == outfile, f"Output file name {outfile_name} does not match expected file name {outfile}."
+                if not os.path.isfile(outfile_name) or os.path.getsize(outfile_name) == 0:
+                    logger.error(f"Output file {outfile_name} after conversion to cmip format was not created successfully.")
+                    sys.exit(1)
+                else:
+                    logger.info(f"File {outfile_name} written.")
+                    os.remove(tmp_outfile_concat)
+                    logger.info(f"Temporary file {tmp_outfile_concat} removed.")
 
-                    tmpfile = extract_vars_change_metadata(nc_file, var, era5_info)
-                    tmpfile_plus1 = extract_vars_change_metadata(nc_file_plus1, var, era5_info)
-
-                    # accumulated values are over the 24 hours ending at 00 UTC i.e. the accumulation is during the previous day,
-                    # so we need to shift the time by -1 second to get the correct calendar day for the accumulation
-                    tmp_outfile = f'{work_out}/{var}_1day_era5-land_{year}{month}.nc'
-                    try:
-                        cdo.shifttime("-1sec", input=tmpfile, output=tmp_outfile)
-                    except Exception as e:
-                        logger.error(f"Error shifting time for {tmpfile}: {e}")
-                        continue
-                    tmp_outfile_plus1 = f'{work_out}/{var}_1day_era5-land_{year}{month}_plus1.nc'
-                    try:
-                        cdo.shifttime("-1sec", input=tmpfile_plus1, output=tmp_outfile_plus1)
-                    except Exception as e:
-                        logger.error(f"Error shifting time for {tmpfile_plus1}: {e}")
-                        continue
-                    # concatenate plus1 file to the end of the month file to get the last day of the month correct
-                    tmp_outfile_concat = f'{work_out}/{var}_1day_era5-land_{year}{month}_concat.nc'
-                    try:
-                        cdo.mergetime(input=f"{tmp_outfile} {tmp_outfile_plus1}", output=tmp_outfile_concat)
-                    except Exception as e:
-                        logger.error(f"Error concatenating {tmp_outfile} and {tmp_outfile_plus1}: {e}")
-                        continue
-
-
-                    outfile = f'{proc_archive}/{era5_info[var]["cmip_name"]}_day_{dataname}_{year}{month}.nc'
-                    outfile_name = convert_era5_to_cmip(
-                        tmp_outfile_concat, outfile, store, era5_info[var],
-                        config['chunking']['time_chk'], config['chunking']['lon_chk'], config['chunking']['lat_chk']
-                    )
-                    assert outfile_name == outfile, f"Output file name {outfile_name} does not match expected file name {outfile}."
-                    if not os.path.isfile(outfile_name) or os.path.getsize(outfile_name) == 0:
-                        logger.error(f"Output file {outfile_name} after conversion to cmip format was not created successfully.")
-                        sys.exit(1)
-                    else:
-                        logger.info(f"File {outfile_name} written.")
-                        os.remove(tmp_outfile_concat)
-                        logger.info(f"Temporary file {tmp_outfile_concat} removed.")
-
-                    # calculate monthly mean
-                    outfile_mon = calc_mon_mean(proc_archive, outfile_name)
-                    if not os.path.isfile(outfile_mon) or os.path.getsize(outfile_mon) == 0:
-                        logger.error(f"Output file {outfile_mon} not created successfully.")
-                        sys.exit(1)
-                    else:
-                        logger.info(f"File {outfile_mon} written successfully.")
+                # calculate monthly mean
+                outfile_mon = calc_mon_mean(proc_archive, outfile_name)
+                if not os.path.isfile(outfile_mon) or os.path.getsize(outfile_mon) == 0:
+                    logger.error(f"Output file {outfile_mon} not created successfully.")
+                    sys.exit(1)
+                else:
+                    logger.info(f"File {outfile_mon} written successfully.")
 
 
 if __name__ == "__main__":
